f12e9dc4bd5dc14d27b238cbd712a17c99cd4652
noah White <nwhite30@knights.ucf.edu>
2022-05-20 22:31:25 -0400

    Set parameter values to persist on close/start

--- src/test/gui_test.py
+++ src/test/gui_test.py
@@ -29,17 +29,18 @@ inertia = DoubleVar(value =.5)
 inertiaBool = BooleanVar(value = False)
 
 
+# Generate path of params file (assumes params.json in same dir as script)
+path = os.path.dirname(__file__) + "/params.json"
+
+# Open the JSON file for reading
+params_file = open(path, mode= "r")
+
+# Convert JSON to dictionary
+params = json.load(params_file)
+
 
 def load_params():
-    # Generate path of params file (assumes params.json in same dir as script)
-    path = os.path.dirname(__file__) + "/params.json"
-
-    # Open the JSON file for reading
-    params_file = open(path, mode= "r")
-
-    # Convert JSON to dictionary
-    params = json.load(params_file)
-
+    
     areaBool.set(params["area"][0])
     area.set(params["area"][1])
 
@@ -55,8 +56,26 @@ def load_params():
     # Close the file
     params_file.close()
 
+    
+
+load_params()
+
+def save_params():
     # Open the JSON file for writing
     params_file = open(path, mode= "w")
+
+    # Update the JSON object with current parameter values
+    params["area"][0] = areaBool.get()
+    params["area"][1] = area.get()
+
+    params["circularity"][0] = circularityBool.get()
+    params["circularity"][1] = circularity.get()
+
+    params["convexivity"][0] = convexivityBool.get()
+    params["convexivity"][1] = convexivity.get()
+
+    params["inertia"][0] = inertiaBool.get()
+    params["inertia"][1] = inertia.get()
 
     # Overwrite the existing JSON file with modified data
     params_file.write(json.dumps(params))
@@ -64,38 +83,30 @@ def load_params():
     # Close the file
     params_file.close()
 
-load_params()
-
-def save_params():
     pass
+
+def on_closing():
+    save_params()
+    window.destroy()
 
 
 # Update each param's value when its corresponding toggle/slider are updated by the user
-def onAreaChange(val): 
-    area.set(val)
-    print(f"area: {area.get()}")
-    
-def onAreaToggle(): 
-    areaBool.set( areaBool.get())
-    print(f"areaBool: {areaBool.get()}")
+def onAreaChange(val): area.set(val)   
+def onAreaToggle(): areaBool.set( areaBool.get())
 
 def onCircChange(val): circularity.set(val)
- 
 def onCircToggle(): circularityBool.set(circularityBool.get())
     
-
-def onConvChange(val): convexivity.set(val)
-    
+def onConvChange(val): convexivity.set(val)  
 def onConvToggle(): convexivityBool.set(convexivityBool.get())
     
-
-def onInertChange(val):inertia.set(val)
-    
+def onInertChange(val):inertia.set(val)  
 def onInertToggle():inertiaBool.set(inertiaBool.get())
     
 
 # Create a new blob detector with given params
 def updateParams(val = -1):
+    # This function will eventually be necessary
     pass
     
 # Create a frame with a label, checkbutton, and slider with specified event-handler functions
@@ -118,7 +129,6 @@ def newParamControlFrame(label, min, max, buttonFunc, sliderFunc, buttonVar, sli
     # Add the button to the right of the label
     cb.pack(side=LEFT, pady = (17,0))
 
-    #sliderVar = IntVar()
     # Create new slider with given function and variable
     paramSlider = Scale(
         frame,
@@ -150,10 +160,11 @@ def buildControlPanel():
     cf4 = newParamControlFrame("Inertia", 0, 100, onInertToggle, onInertChange, inertiaBool, inertia)
     cf4.grid(row=3)
 
-#labels = ["Area", "Circularity", "Convexivity", "Inertia"]
-
 # Add all the frames to the window
 buildControlPanel()
+
+# Set the current params to be saved on window close
+window.protocol("WM_DELETE_WINDOW", on_closing)
 
 # Run the gui
 window.mainloop()
